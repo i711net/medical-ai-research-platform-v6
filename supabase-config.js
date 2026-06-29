5ec3d4ca41759f781ceceeddc46a8e06eee29e25
i711net <i711net@gmail.com>
2026-06-29 18:48:01 +0800

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.SUPABASE_CONFIG = {
-  url: "https://wvvjiavpxllugcoatsoq.supabase.co/rest/v1/",
+  url: "https://wvvjiavpxllugcoatsoq.supabase.co",
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2dmppYXZweGxsdWdjb2F0c29xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3MjA2ODYsImV4cCI6MjA5ODI5NjY4Nn0.jzZuZxmgksk2AWve7VF-sTFO7_15bScJFhxiWuR2nYE
 "
 };
